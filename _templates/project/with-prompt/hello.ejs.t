552bd081098e9a128b27fbc77c3d8ef132bef0fa
Michael Sitchenko <michael.sitchenko@gmail.com>
2020-09-03 19:10:28 -0400

    restructure

--- _templates/project/with-prompt/hello.ejs.t
+++
@@ -1,9 +0,0 @@
-
-to: README.md
-sh: curl -s https://www.gitignore.io/api/archive,macos,linux,windows,node,jetbrains,sublimetext,eclipse,netbeans,visualstudiocode > .gitignore
----
-# <%= name %>
-
-<%= message %>
-
-
--- _templates/project/with-prompt/prompt.js
+++ _templates/project/with-prompt/prompt.js
@@ -1,29 +1,46 @@
+const execa = require("execa")
+
 // see types of prompts:
 // https://github.com/enquirer/enquirer/tree/master/examples
 //
 
+function sh(cmd, opts = []) {
+  const { stdout } = execa.sync(cmd, opts)
+  return stdout
+}
+
 function getUserName() {
-  const cmd = "git config --get user.name"
-  return cmd
+  return sh("git", ["config", "--get", "user.name"])
 }
 function getUserEmail() {
-  const cmd = "git config --get user.email"
-  return cmd
+  return sh("git", ["config", "--get", "user.email"])
 }
 function getRemoteOriginUrl() {
-  const cmd = "git config --get remote.origin.url"
-  return cmd
+  return sh("git", ["config", "--get", "remote.origin.url"])
 }
-function getPackage() {
-  const url = getRemoteOriginUrl()
-  return url.split("/").pop()
+function getPackageName() {
+  console.log(getRemoteOriginUrl())
+  return getRemoteOriginUrl().split("/").pop()
 }
 
 module.exports = [
   {
     type: "input",
-    name: "name",
+    name: "package",
     message: "What's the name of your package?",
+    initial: getPackageName(),
+  },
+  {
+    type: "input",
+    name: "author",
+    message: "What's your full name?",
+    initial: getUserName(),
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What's your email?",
+    initial: getUserEmail(),
   },
   {
     type: "input",
